22fec0bcbbd79a5e4e0f44e3889e176d824603fc
Goku <francisco.perezca.13@sansano.usm.cl>
2019-03-25 15:31:27 -0300

    Product CRUD 100%
    - Default crud working with twig

--- project/app.js
+++ project/app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var products = require('./routes/product');
 
 var mongoose = require('mongoose');
 var mongoDB = 'mongodb://127.0.0.1:27017/mydb';
@@ -30,6 +31,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/product', products);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
